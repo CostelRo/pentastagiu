589727c988974dbbdc6835e0a7de3e94dc34d448
Costelro <costelro10@gmail.com>
2019-01-31 16:16:22 +0200

    Homework #7 (v.0.5) - useless file, probably

--- src/main/java/airportmanager/springjavaconfig/AirportManagerConfig.java
+++ src/main/java/airportmanager/springjavaconfig/AirportManagerConfig.java
@@ -1,24 +1,20 @@
 package airportmanager.springjavaconfig;
 
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-
-@Configuration
-@ComponentScan( basePackages = "airportmanager" )
-public class AirportManagerConfig
-{
-//    @Bean( name = "localAirport" )
-//    public Airport airport( String airportCode, String city, String country )
-//    {
-//        if( airportCode != null && city != null && country != null )
-//        {
-//            return new Airport( "IAS", "Iasi", "Romania");
-//        }
-//        else
-//        {
-//            return new Airport( airportCode, city, country );
-//        }
-//    }
-}
+//@Configuration
+//@ComponentScan( basePackages = "airportmanager" )
+//public class AirportManagerConfig
+//{
+////    @Bean( name = "localAirport" )
+////    public Airport airport( String airportCode, String city, String country )
+////    {
+////        if( airportCode != null && city != null && country != null )
+////        {
+////            return new Airport( "IAS", "Iasi", "Romania");
+////        }
+////        else
+////        {
+////            return new Airport( airportCode, city, country );
+////        }
+////    }
+//}
