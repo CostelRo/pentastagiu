469562e2520be1af6349a1cfc99c936961c63fd0
Costelro <costelro10@gmail.com>
2019-02-14 16:37:30 +0200

    Homework #8 (v0.5) - Spring + Hibernate with annotations (work in progress)

--- src/main/java/airportmanager/model/FlightStatusEntity.java
+++ src/main/java/airportmanager/model/FlightStatusEntity.java
@@ -1,7 +1,12 @@
 package airportmanager.model;
 
 
+import airportmanager.FlightStatus;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
 
 
@@ -9,5 +14,38 @@ import javax.persistence.Table;
 @Table( name = "t_flight_status" )
 public class FlightStatusEntity extends AbstractBaseEntity
 {
+    // fields
 
+    @Column( name = "status" )
+    @Enumerated( EnumType.STRING )
+    private FlightStatus status;
+
+
+    // constructors
+
+    public FlightStatusEntity()
+    {
+        // default entity constructor
+    }
+
+    public FlightStatusEntity( FlightStatus status )
+    {
+        this.status = status;
+    }
+
+
+    // getters & setters
+
+    public FlightStatus getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus( FlightStatus status )
+    {
+        if( status != null )
+        {
+            this.status = status;
+        }
+    }
 }
